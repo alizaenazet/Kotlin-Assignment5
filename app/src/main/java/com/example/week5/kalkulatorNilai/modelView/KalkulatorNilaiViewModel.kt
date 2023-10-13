0d6d6eccae4651e0e897fffcb81f08713fa5154c
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-10-13 13:16:18 +0700

    refactor: Kalkulator nilai following best practice for MVVM

--- app/src/main/java/com/example/week5/kalkulatorNilai/modelView/KalkulatorNilaiViewModel.kt
+++ app/src/main/java/com/example/week5/kalkulatorNilai/modelView/KalkulatorNilaiViewModel.kt
@@ -4,51 +4,111 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.week5.kalkulatorNilai.model.Courses
 import com.example.week5.kalkulatorNilai.model.Subject
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
-sealed interface KalkulatorNilaiUIState{
-    data class Success(val data: Courses):KalkulatorNilaiUIState
-    object Error: KalkulatorNilaiUIState
-    object Loading: KalkulatorNilaiUIState
-}
+
+
+
+
 
 class KalkulatorNilaiViewModel: ViewModel() {
 
-    var kalkulatorNilaiUIState: KalkulatorNilaiUIState by mutableStateOf(KalkulatorNilaiUIState.Loading)
-        private set
+    private val _uiState = MutableStateFlow(KalkulatorNilaiUiState())
+    val uiState: StateFlow<KalkulatorNilaiUiState> = _uiState.asStateFlow()
 
     private lateinit var data: Courses
 
+    var userSksInput by mutableStateOf("")
+        private set
+    var userScoreInput by mutableStateOf("")
+        private set
+    var userNameInput by mutableStateOf("")
+        private set
+
+
     init {
-        loadData()
+        startPage()
     }
 
-    fun loadData(){
-        viewModelScope.launch {
-            try {
+    private fun startPage(){
                 data = Courses(0,0.00)
-                kalkulatorNilaiUIState = KalkulatorNilaiUIState.Success(data)
-            }catch (e: Exception){
-                kalkulatorNilaiUIState = KalkulatorNilaiUIState.Error
+                _uiState.value = KalkulatorNilaiUiState(
+                    ipk = 0.00,
+                    sksTotal = 0,
+                    subjects = HashMap<String, Subject>()
+                    )
+    }
+
+    private fun addNewSubject(name: String, sks: Int, score: Double){
+        data.addSubject(Subject(name,sks,score))
+        data.calculateCourse()
+        var tempSubject : HashMap<String, Subject> = uiState.value.subjects
+        tempSubject.put(name, Subject(name,sks,score))
+        _uiState.update { currentState-> currentState.copy(subjects = tempSubject) }
+        calculateCourse()
+    }
+
+    private fun calculateCourse(){
+        if (uiState.value.subjects.isNotEmpty()) {
+            var tempSksTotal = 0
+            var tempScoreTotal: Double = 0.00
+
+            uiState.value.subjects.forEach { name, subject ->
+                tempSksTotal += subject.sks
+                tempScoreTotal += (subject.score * subject.sks)
+            }
+
+            if (tempSksTotal == 0) {
+                _uiState.update { currentState ->
+                    currentState.copy(sksTotal = 0, ipk = 0.00)
+                }
+            } else {
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        sksTotal = tempSksTotal,
+                        ipk = tempScoreTotal
+                    )
+                }
+            }
+        }else{
+            _uiState.update { currentState ->
+                currentState.copy(sksTotal = 0, ipk = 0.00)
             }
         }
     }
-
-    fun addNewSubject(name: String, sks: Int, score: Double){
-        data.addSubject(Subject(name,sks,score))
-        data.calculateCourse()
-    }
-
     fun removeSubject(name: String){
         data.deleteSubject(name)
         data.calculateCourse()
+        var tempSubject : HashMap<String, Subject> = uiState.value.subjects
+        tempSubject.remove(name)
+        _uiState.update { currentState-> currentState.copy(
+            subjects = tempSubject
+        ) }
+        calculateCourse()
     }
 
-    fun getData(): Courses{
-        return data
+    fun onSksInputChange(sksInput: String){
+        userSksInput = sksInput
+    }
+
+    fun onScoreInputChange(scoreInput: String){
+        userScoreInput = scoreInput
+    }
+
+    fun onNameInputChange(nameInput: String){
+        userNameInput = nameInput
+    }
+
+    fun onAddSubjectClik(){
+        addNewSubject(userNameInput,userSksInput.toInt(),userScoreInput.toDouble())
+        userNameInput = ""
+        userScoreInput = ""
+        userSksInput = ""
     }
 
 }
